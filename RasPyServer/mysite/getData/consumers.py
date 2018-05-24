cb385214e5d8dfc7688d60e7f3f9fb147b791030
yves-weissenberger <yvesweissenberger@gmail.com>
2018-05-24 15:54:45 +0100

    changed page updater to be asychrnous

--- RasPyServer/mysite/getData/consumers.py
+++ RasPyServer/mysite/getData/consumers.py
@@ -1,26 +1,31 @@
 from channels.generic.websocket import WebsocketConsumer
+from channels.generic.websocket import AsyncWebsocketConsumer
 import json
+import asychio
 import os
 
-class ChatConsumer(WebsocketConsumer):
-    def connect(self):
-        self.accept()
+class ChatConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        await self.accept()
 
-    def disconnect(self, close_code):
+    async def disconnect(self, close_code):
         pass
 
-    def receive(self, text_data):
-        imgF = '/Users/Yves/Documents/Code/behaviour_webserver/RasPyServer/mysite/getData/static/getData/ims/'
-        image_store = sorted(os.listdir(imgF))
-        text_data_json = json.loads(text_data)
-        message = (int(text_data_json['message']) + 1) % 60
-        try:
-            img_loc = image_store[message]
+    async def receive(self, text_data):
+
+        while True:
+            await asyncio.sleep(0.1)
+            imgF = '/Users/Yves/Documents/Code/behaviour_webserver/RasPyServer/mysite/getData/static/getData/ims/'
+            image_store = sorted(os.listdir(imgF))
+            text_data_json = json.loads(text_data)
+            message = (int(text_data_json['message']) + 1) % 60
+            try:
+                img_loc = image_store[message]
 
 
-            self.send(text_data=json.dumps({
-                'message': message,
-                'img_loc': img_loc
-            }))
-        except:
-            pass
+                await self.send(text_data=json.dumps({
+                    'message': message,
+                    'img_loc': img_loc
+                }))
+            except:
+                pass
